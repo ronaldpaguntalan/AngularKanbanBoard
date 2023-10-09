68fe35a4880d3f6659a0ee68e4a6c322ae354321
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-10-09 22:09:26 +0800

    Kanban Board

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,44 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ITask } from '../models/task';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss'],
+})
+export class TodoComponent {
+  todoForm!: FormGroup;
+  tasks: ITask[] = [];
+  inprogress: any[] = [];
+  done: any[] = [];
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item: ['', Validators.required],
+    });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
