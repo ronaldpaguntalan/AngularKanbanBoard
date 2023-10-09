ffb17a6071dc658e95011b7e44e71c9f03bd714b
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-10-10 03:16:23 +0800

    Done Kanban Board

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -15,8 +15,10 @@ import { ITask } from '../models/task';
 export class TodoComponent {
   todoForm!: FormGroup;
   tasks: ITask[] = [];
-  inprogress: any[] = [];
-  done: any[] = [];
+  inprogress: ITask[] = [];
+  done: ITask[] = [];
+  updateIndex!: any;
+  isEditEnabled: boolean = false;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
@@ -25,7 +27,41 @@ export class TodoComponent {
     });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  addTask(){
+    this.tasks.push({
+      description:this.todoForm.value.item, 
+      done: false
+    })
+    this.todoForm.reset();
+  }
+
+  deleteTask(i : number){
+    this.tasks.splice(i, 1);
+  }
+
+  deleteInProgressTask(i : number){
+    this.inprogress.splice(i, 1);
+  }
+
+  deleteDoneTask(i : number){
+    this.done.splice(i, 1);
+  }
+
+  onEdit(item: ITask, i: number){
+    this.todoForm.controls['item'].setValue(item.description);
+    this.updateIndex = i;
+    this.isEditEnabled = true;
+  }
+
+  updateTask(){
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.isEditEnabled = false;
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
